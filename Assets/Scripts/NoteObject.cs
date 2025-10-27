245934e024366a0bab44dde2b293d0658bd5721f
✿ オレクサンドラ ✿ <zuzukochkame@gmail.com>
2025-10-27 14:58:21 +0900

    start game mode from tutorial

---
+++ Assets/Scripts/NoteObject.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+    public KeyCode keyToPress;
+
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+    public Transform targetObjectTransform;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(keyToPress)) {
+            if (canBePressed) { 
+                gameObject.SetActive(false);
+
+                if(Mathf.Abs(transform.position.y) > 0.25)
+                {
+                    GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, targetObjectTransform.position, targetObjectTransform.rotation);
+                } else if (Mathf.Abs(transform.position.y) > 0.05)
+                {
+                    GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, targetObjectTransform.position, targetObjectTransform.rotation);
+                } else
+                {
+                    GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, targetObjectTransform.position, targetObjectTransform.rotation);
+                }
+            }
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = true;
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Activator")
+        {
+            canBePressed = false;
+
+            GameManager.instance.NoteMissed();
+            Instantiate(missEffect, targetObjectTransform.position, targetObjectTransform.rotation);
+        }
+    }
+}
